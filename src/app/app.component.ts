13dc1d6623248b2757b0ec26a637adffbda319c7
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-05-22 23:16:41 -0300

    fix: corrigindo módulo de import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importe aqui
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet
+  imports: [
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { CustomersComponent } from './features/customers/customers.component';
+import { CreateCustomerComponent } from './features/create-customer/create-customer.component';
 
 export const routes: Routes = [
 
@@ -12,5 +12,9 @@ export const routes: Routes = [
     {
         path: 'clients',
         component: CustomersComponent
+    },
+    {
+        path: 'adicionar-cliente',
+        component: CreateCustomerComponent
     }
 ];
