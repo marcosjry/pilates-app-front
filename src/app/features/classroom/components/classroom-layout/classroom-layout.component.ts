2169f7c01c314ef5487a11190bc71a7ffa7b4372
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-05-28 21:40:54 -0300

    feat: criado layout pra página de turmas

---
+++ src/app/features/classroom/components/classroom-layout/classroom-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ClassroomNavbarComponent } from "../classroom-navbar/classroom-navbar.component";
+
+@Component({
+  selector: 'app-classroom-layout',
+  imports: [ClassroomNavbarComponent],
+  templateUrl: './classroom-layout.component.html',
+  styleUrl: './classroom-layout.component.scss'
+})
+export class ClassroomLayoutComponent {
+
+}
