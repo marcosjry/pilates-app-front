f051d4bf7dae6cf00f9449c1f5b992610ed136bd
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-06-03 18:41:55 -0300

    feat: adicionado interceptor de autenticação para token jwt

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,30 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getUserToken();
+  const router = inject(Router);
+
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
+  return next(req).pipe(
+    catchError((err) => {
+      if (err.status === 401 || err.status === 403) {
+        authService.onLogout();
+        router.navigate(['']);
+      }
+
+      return throwError(() => err);
+    })
+  );
+};
