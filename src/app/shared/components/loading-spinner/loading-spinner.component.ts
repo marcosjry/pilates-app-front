03b460537c66b76ce9b22418b8daaf89700f35bc
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-06-03 19:10:27 -0300

    refactor: loading spinner e o button agr recebem input do componente pai

--- src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
-import { LoadingService } from '../../services/loading.service';
 
 @Component({
   selector: 'app-loading-spinner',
@@ -14,10 +13,9 @@ import { LoadingService } from '../../services/loading.service';
 })
 export class LoadingSpinnerComponent {
 
-  isLoading: boolean = false
+  @Input() isLoading: boolean = false
   @Input() spinnerSize!: number
 
-  constructor(private loadingService: LoadingService) {
-    this.loadingService.isLoading$.subscribe(value => this.isLoading = value);
+  constructor() {
   }
 }
